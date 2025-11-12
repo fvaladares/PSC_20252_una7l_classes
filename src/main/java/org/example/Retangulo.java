846d40b04d41364833a4ac0d75e6a905f01b84b2
Fabricio Valadares <fabricio.valadares@gmail.com>
2025-11-11 21:25:17 -0300

    First commit

--- src/main/java/org/example/Retangulo.java
+++ src/main/java/org/example/Retangulo.java
@@ -4,6 +4,21 @@ public class Retangulo {
     private double base;
     private double altura;
 
+    /**
+     * Construtor da classe - utilizado na inicialização do objeto.
+     * @param base
+     * @param altura
+     */
+    public Retangulo(double base,
+                     double altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    /**
+     *
+     * @return o valor do perímeto do retângulo
+     */
     public double perimetro() {
         // realiza o cálculo e retorna o valor
         double resultado = 0;
@@ -13,16 +28,12 @@ public class Retangulo {
 
     public double area() {
         // realiza o cálculo e retorna o valor
-        double resultado = 0;
-
-        return resultado;
+        return 0;
     }
 
     public double diagonal() {
         // realiza o cálculo e retorna o valor
-        double resultado = 0;
-
-        return resultado;
+        return 0;
     }
 
 }
